4d4aafd9b5a45436934a10a5a17f9cce0c7e89ad
Kyle Mayer <x@kylemayer.dev>
2021-07-19 16:32:31 -0700

    captalizeAndFilter + fetchQuotes tests described

---
+++ capt-filter.test.js
@@ -0,0 +1,5 @@
+describe('capitalizeAndFilter', () => {
+  it('returns an array of capitalized strings and any strings starting with the letter F/f are filtered out', async () => {
+
+  });
+});
---
+++ fetch-quotes.test.js
@@ -0,0 +1,5 @@
+describe('fetchQuotes', () => {
+  it('returns a single quote from the Futurama Quote API', async () => {
+
+  });
+});
