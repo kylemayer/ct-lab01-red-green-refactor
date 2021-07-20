a7a2bd4833daba960fc0ea82135adf5af3b607e1
Kyle Mayer <x@kylemayer.dev>
2021-07-20 09:47:34 -0700

    red test status - capitalizeAndFilter

--- capt-filter.test.js
+++ capt-filter.test.js
@@ -1,5 +1,9 @@
 describe('capitalizeAndFilter', () => {
   it('returns an array of capitalized strings and any strings starting with the letter F/f are filtered out', async () => {
+    const arr = ['coffee', 'tadpole', 'fresh', 'floral', 'gear', 'fig'];
+    const expected = ['COFFEE', 'TADPOLE', 'GEAR'];
+    const actual = capitalizeAndFilter(arr);
 
+    expect(actual).toEqual(expected);
   });
 });
