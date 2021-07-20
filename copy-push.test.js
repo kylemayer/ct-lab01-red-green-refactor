332f5140d5c6cfd82e9043f6d2eea74f27804f25
Kyle Mayer <x@kylemayer.dev>
2021-07-20 09:22:49 -0700

    red - copyAndPush

--- copy-push.test.js
+++ copy-push.test.js
@@ -1,5 +1,9 @@
 describe('copyAndPush', () => {
   it('returns a new array with all of the items in the original array and a new item pushed to the end', async () => {
+    const numbers = [1, 2, 3];
+    const expected = [1, 2, 3, 4];
+    const actual = copyAndPush (numbers, 4);
 
+    expect(actual).toEqual(expected);
   });
 });
