c68707fee04e73a0aabfb6cfd86048207303db76
Kyle Mayer <x@kylemayer.dev>
2021-07-19 17:07:51 -0700

    getName file created, red test status

--- get-name.test.js
+++ get-name.test.js
@@ -1,5 +1,12 @@
+import getName from './get-name';
+
 describe('getName', () => {
   it('returns the name property of the object', async () => {
 
+    const spot = 'spot';
+    const expected = { name: 'spot' };
+    const actual = getName(spot);
+
+    expect(actual).toEqual(expected);
   });
 });
